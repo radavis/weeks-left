722112dec4ba31a1344790642cac7c7bca6fe1ed
radavis <rdavis.bacs@gmail.com>
2021-09-28 17:30:25 -0400

    tech demo

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const DAYS_PER_YEAR = 365.25;
 const MILLISECONDS_PER_WEEK = 1000 * 60 * 60 * 24 * 7;
+const SIGNIFICANT_DIGITS = 6;
 
 const weeksLeft = (birthdate, expectedLifetimeInYears) => {
   let endDate = new Date(birthdate);
-  let expectedLifetimeInDays = Math.round(expectedLifetimeInYears * DAYS_PER_YEAR)
+  let expectedLifetimeInDays = expectedLifetimeInYears * DAYS_PER_YEAR;
   endDate.setDate(endDate.getDate() + expectedLifetimeInDays);
 
   let lifeLeftInMilliSeconds = endDate - new Date();
-  return lifeLeftInMilliSeconds / MILLISECONDS_PER_WEEK;
+  let wholeWeeks = Math.floor(lifeLeftInMilliSeconds / MILLISECONDS_PER_WEEK);
+  let milliseconds = lifeLeftInMilliSeconds % MILLISECONDS_PER_WEEK;
+  let fractionalWeeks = Math.pow(10, SIGNIFICANT_DIGITS) * milliseconds / MILLISECONDS_PER_WEEK;
+  fractionalWeeks = Math.floor(fractionalWeeks);
+
+  return [wholeWeeks, fractionalWeeks];
 }
 
 const updateContent = () => {
   // https://www.lifeexpectancy.org/asp/Calculator/default.asp
-  let result = weeksLeft("1981-03-31", 78.7);
+  let [wholeWeeks, fractionalWeeks] = weeksLeft("1981-03-31", 78.7);
 
   let html = `<h1>`;
-  html += result.toFixed(6);
-  html += ` weeks left`;
+  html += `You Have `;
+  html += `${wholeWeeks}.`;
+  html += `<span class='small-text'>${fractionalWeeks}</span>`
+  html += ` Weeks Left`;
   html += `</h1>`;
 
   let content = document.getElementById("content");
@@ -24,7 +32,7 @@ const updateContent = () => {
 }
 
 const main = () => {
-  setInterval(updateContent);
+  setInterval(updateContent, 100);
 }
 
 document.addEventListener("DOMContentLoaded", main);
